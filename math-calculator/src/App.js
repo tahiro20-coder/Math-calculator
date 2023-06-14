993073b333bde2448e322d03abe82a9e4020814d
tahiro20-coder <rezzag2016@gmail.com>
2023-06-14 22:44:05 +0100

    Removed Header from App.js

--- math-calculator/src/App.js
+++ math-calculator/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Taher_components/Header.js';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Header/>
         </p>
         
         <a
