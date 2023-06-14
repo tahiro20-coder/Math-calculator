f9bd5877fc10a2b845d46effcdec4fa6fcab3e47
tahiro20-coder <rezzag2016@gmail.com>
2023-06-14 14:12:02 +0100

    Remove Taher components now dev is the same as main

--- math-calculator/src/App.js
+++ math-calculator/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button>this is button</Button>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
