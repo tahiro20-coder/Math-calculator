75ee5ebebfa8c2b22aae5dc7373155dd185ffb0c
tahiro20-coder <rezzag2016@gmail.com>
2023-07-11 00:11:18 +0100

    Update App.js

--- math-calculator/src/App.js
+++ math-calculator/src/App.js
@@ -1,8 +1,8 @@
 import '../src/Styles/App.css';
 import "./Styles/Fonts.css";
-import Header from './Taher_Components/Header.js';
-import Footer from './Taher_Components/Footer.js';
-import PageContent from './Taher_Components/PageContent.js';
+import Header from './Taher_components/Header.js';
+import Footer from './Taher_components/Footer.js';
+import PageContent from './Taher_components/PageContent.js';
 import React from 'react'
 
 
